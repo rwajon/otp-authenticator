1c2d14981e5969878d87312621c8052bf9ce674b
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2023-03-18 14:45:45 +0200

    add endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,93 @@
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use dotenv;
+use serde::{Deserialize, Serialize};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {req:?}");
-    "Hello world!"
+#[derive(Serialize)]
+struct GenerateSecret {
+    secret: String,
+    uri: String,
+    qr_code: String,
+}
+
+#[derive(Deserialize)]
+struct GenerateOTP {
+    secret: String,
+    encoding: Option<String>,
+}
+
+#[derive(Serialize)]
+struct GenerateOTPResponse {
+    token: String,
+    period: u32,
+}
+
+#[derive(Deserialize)]
+struct VerifyOTP {
+    secret: String,
+    token: String,
+    encoding: Option<String>,
+}
+
+#[derive(Serialize)]
+struct VerifyOTPResponse {
+    is_valid: bool,
+    period: u32,
+    message: String,
+}
+
+async fn generate_secret(req: HttpRequest) -> Result<HttpResponse, Error> {
+    let result = GenerateSecret {
+        secret: "secret".to_string(),
+        uri: "uri".to_string(),
+        qr_code: "qr_code".to_string(),
+    };
+    Ok(HttpResponse::Created().json(result))
+}
+
+async fn generate_otp(payload: web::Json<GenerateOTP>) -> Result<HttpResponse, Error> {
+    println!("secret {}", payload.secret);
+    println!("encoding {:?}", payload.encoding);
+    let result = GenerateOTPResponse {
+        token: "token".to_string(),
+        period: 30,
+    };
+    Ok(HttpResponse::Created().json(result))
+}
+
+async fn validate_otp(payload: web::Json<VerifyOTP>) -> Result<HttpResponse, Error> {
+    println!("secret {}", payload.secret);
+    println!("encoding {:?}", payload.encoding);
+    let result = VerifyOTPResponse {
+        is_valid: true,
+        period: 30,
+        message: "OTP is valid".to_string(),
+    };
+    Ok(HttpResponse::Ok().json(result))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let port = match dotenv::var("PORT") {
+        Ok(val) => val.to_string(),
+        Err(_) => "5000".to_string(),
+    };
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    log::info!("starting HTTP server at http://localhost:5555");
+    log::info!("starting HTTP server at http://localhost:{}", port);
 
     HttpServer::new(|| {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
+            .service(web::resource("/index.html").to(|| async { "Authenticator!" }))
+            .route("/api/otp/secret", web::get().to(generate_secret))
+            .route("/api/otp/generate", web::post().to(generate_otp))
+            .route("/api/otp/validate", web::post().to(validate_otp))
     })
-    .bind(("127.0.0.1", 5555))?
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
-}
-
-#[cfg(test)]
-mod tests {
-    use actix_web::{body::to_bytes, dev::Service, http, test, web, App, Error};
-
-    use super::*;
-
-    #[actix_web::test]
-    async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let app = test::init_service(app).await;
-
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await?;
-
-        assert_eq!(resp.status(), http::StatusCode::OK);
-
-        let response_body = resp.into_body();
-        assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
-
-        Ok(())
-    }
 }
